ea3d415e5770bee70dee58c255d9473871fe8b0c
Adolfo Agnelli <adolfoagnelli@gmail.com>
2022-02-12 18:57:01 -0300

    addOperatorUnitysDescription

---
+++ expressoes e operadores/expressionoperators.js
@@ -0,0 +1,27 @@
+/* Expressoes e Operadores
+
+- Expressions
+- Operators
+    Binary
+    Unary
+    Ternary
+*/
+
+let number = 1
+//binario
+console.log(number + 1)
+//Unary
+console.log(--number)
+//Ternary
+console.log(false ? 'alo' : 'nada')
+
+// new = criar um novo objeto
+
+let date = new Date('2020-12-01')
+console.log(date.__proto__)
+
+/* 
+    Operadores Unarios
+    typeof
+    delete
+*/
